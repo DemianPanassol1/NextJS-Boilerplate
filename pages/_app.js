bfb6de92ef4f9a57f6aefad30cd006056adc7115
DemianPanassol1 <demianpanassol01@gmail.com>
2022-03-25 22:10:13 -0300

    1° commit

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import 'normalize.css';
+import '../styles/globals.scss';
+
+function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />;
+};
+
+MyApp.propTypes = {
+	Component: PropTypes.oneOfType([PropTypes.func, PropTypes.object])
+		.isRequired,
+	pageProps: PropTypes.shape({}).isRequired,
+};
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Home = () => {
+
+	return (
+		<div>
+			<h1>Página Pricipal</h1>
+		</div>
+	);
+};
+
+// Home.propTypes = {};
+
+export default Home;
